573a345ef43b9a82d2d7a39234b6b5b805b4dd28
Mitchell Petellin <petemi09@Mitchells-MacBook-Pro-2.local>
2018-04-09 22:22:15 -0500

    google places api

--- location.js
+++ location.js
@@ -1,28 +1,40 @@
+var map;
+var infowindow;
 
-
-function clickedon() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+function initMap() {
+  var pyrmont = {lat: 43.3111, lng: -91.8036};
 
   map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
+    center: pyrmont,
+    zoom: 15
+  });
 
-  var request = {
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
     location: pyrmont,
-    radius: '500',
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+    radius: 500,
+    type: ['food']
+  }, callback);
 }
 
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      var place = results[i];
       createMarker(results[i]);
     }
   }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
