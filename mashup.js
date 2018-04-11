8e7ebf711c9e3c7052fb283f3358ea430c60e9a5
[millan04] <[millan04@luther.edu]>
2018-04-10 20:31:48 -0500

    location located

--- mashup.js
+++ mashup.js
@@ -1,6 +1,8 @@
 "use strict;"
 var map;
 var infowindow;
+var lati;
+var long;
 
 
 function initMap(){ 	
@@ -16,6 +18,39 @@ function clickedon() {
 
 	let city = document.getElementById("city").value
 	let state = document.getElementById("state").value
+
+	let lo = document.getElementById("location")
+
+	lo.innerHTML = city + ", " + state
+
+	let location = city + ", " + state
+
+	
+
+	var geocoder =  new google.maps.Geocoder();
+    geocoder.geocode( { 'address': location}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+          	lati = results[0].geometry.location.lat();
+            long = results[0].geometry.location.lng();
+            var pyrmont = {lat: lati, lng: long};
+
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: pyrmont,
+			    zoom: 13
+			  });
+
+			infowindow = new google.maps.InfoWindow();
+			var service = new google.maps.places.PlacesService(map);
+			service.nearbySearch({
+				location: pyrmont,
+			    radius: 5000,
+			    type: ['restaurant']
+	  		}, callback);
+          } else {
+            alert("Something got wrong " + status);
+          }
+        });
+
 	let co = document.getElementById("city_output")
 	let so = document.getElementById("state_output")
 
@@ -101,4 +136,4 @@ function createMarker(place) {
 	//       }
 	//     });
 	//   }
-// }
+	// }
